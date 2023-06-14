8fd5dc9a984a5e509c11aa8479efc73aa70d257d
Documenter.jl <documenter@juliadocs.github.io>
2023-06-14 04:04:49 +0000

    build based on 8f99d21

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GemmCUDA","category":"page"},{"location":"#GemmCUDA","page":"Home","title":"GemmCUDA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GemmCUDA.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GemmCUDA]","category":"page"}]
+}
